782941ee6537e717bf46215b5ac50523bf0927bf
Júnior Fernandes <juniorfernandes282@gmail.com>
2021-06-15 22:31:17 -0300

    first commit

---
+++ src/main/java/com/example/cobranca/controller/TituloController.java
@@ -0,0 +1,45 @@
+package com.example.cobranca.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.cobranca.model.StatusTitulo;
+import com.example.cobranca.model.Titulo;
+import com.example.cobranca.repository.TituloRepository;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	@Autowired(required=true)
+	private TituloRepository tituloRepository; 
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		System.out.println("Titulo " + titulo.getDescricao());
+		tituloRepository.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+
+}
---
+++ src/main/java/com/example/cobranca/repository/TituloRepository.java
@@ -0,0 +1,11 @@
+package com.example.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.cobranca.model.Titulo;
+
+@Repository
+public interface TituloRepository extends JpaRepository<Titulo, Long> {
+	
+}
